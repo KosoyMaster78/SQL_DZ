2c83161406ce22e87d4e46cbf283cfda14cdac21
Ilya Eliseev <black.oxygen.51@gmail.com>
2024-01-10 13:50:59 +0300

    Reworked Homework

--- PycharmProjects/project_test/main.py
+++ PycharmProjects/project_test/main.py
@@ -9,8 +9,7 @@ class Student:
         self.courses_attached = []
 
     def rate_lecturer(self, lector, course, grade):
-        if (isinstance(lector, Lecturer) and course in lector.courses_attached and course in self.courses_in_progress
-                and grade in range(0, 11)):
+        if isinstance(lector, Lecturer) and course in lector.courses_attached and course in self.courses_in_progress:
             if course in lector.grades:
                 lector.grades[course] += [grade]
             else:
@@ -18,19 +17,25 @@ class Student:
         else:
             return 'Ошибка'
 
+    def __lt__(self,other):
+        if self.average_grade() < other.average_grade():
+            return (f"Средняя оценка {self.name} ниже чем у {other.name}")
+        else:
+            return (f"Средняя оценка {self.name} выше чем у {other.name}")
+
     def average_grade(self):
         result = 0
         if self.grades:
             for grade in self.grades.values():
-                result += sum(grade) / len(grade)
+                result += sum(grade) // len(grade)
             return result / len(self.grades)
         else:
             return 0
 
     def __str__(self):
-        return (f"Имя:{self.name}\n Фамилия:{self.surname}\n Средняя оценка за домашние задания:"
-                f"{self.average_grade()}\n Курсы в процессе изучения:{self.courses_in_progress}\n Завершенные курсы:"
-                f"{self.finished_courses}")
+        return (f"Имя:{self.name}\n Фамилия:{self.surname}\n Оценки:{self.grades}\n Средняя оценка за домашние задания:"
+                f"{self.average_grade()}\n  Курсы в процессе изучения:{self.courses_in_progress}\n Завершенные курсы:"
+                f"{self.finished_courses}\n\n")
 
 class Mentor:
     def __init__(self, name, surname):
@@ -41,21 +46,27 @@ class Mentor:
 
 
 
-
 class Lecturer(Mentor):
 
     def average_grade(self):
         result = 0
         if self.grades:
             for grade in self.grades.values():
-                result += sum(grade) / len(grade)
+                result += sum(grade) // len(grade)
             return result / len(self.grades)
         else:
             return 0
 
     def __str__(self):
-        return (f"Имя:{self.name}\n Фамилия:{self.surname}\n Средняя оценка за лекцию:"
+        return (f"Имя:{self.name}\n Фамилия:{self.surname}\n Оценки от студентов:{self.grades}\n Средняя оценка "
+                f"за лекцию:"
                 f"{self.average_grade()}\n")
+
+    def __lt__(self,other):
+        if self.average_grade() < other.average_grade():
+            return (f"Средняя оценка {self.name} ниже чем у {other.name}")
+        else:
+            return (f"Средняя оценка {self.name} выше чем у {other.name}")
 
 
 
@@ -74,30 +85,52 @@ class Reviewer(Mentor):
 
 
 best_student1 = Student('Ruoy', 'Eman', 'woman')
-best_student2 = Student('Rick','Morty','male')
-best_student3 = Student( 'Alf','Alfik', 'Ufo')
+best_student2 = Student('Rick', 'Morty', 'male')
+best_student3 = Student('Alf', 'Alfik', 'Ufo')
 
 best_student1.finished_courses += ['First step on Python']
 best_student2.finished_courses += ['First step on Python']
 best_student3.finished_courses += ['First step on Python']
 
+
 cool_mentor = Reviewer('Erick', 'Pedigree')
-cool_mentor.courses_attached += ['Python'], ['Java'], ['CSharp'], ['Pascal']
+cool_mentor.courses_attached += ['Python']
+cool_mentor.courses_attached += ['Java']
+cool_mentor.courses_attached += ['CSharp']
+cool_mentor.courses_attached += ['Pascal']
+cool_mentor.courses_attached += ['C++']
 
-cool_lector = Lecturer('Jonh', 'Snow')
-cool_lector.courses_attached += ['Python'], ['Java'], ['CSharp'], ['Pascal']
+cool_lector = Lecturer('John', 'Snow')
+cool_lector.courses_attached += ['Python']
+cool_lector.courses_attached += ['Java']
+cool_lector.courses_attached += ['CSharp']
+cool_lector.courses_attached += ['Pascal']
+
+cool_lector1 = Lecturer('Luke', 'Skywalker')
+cool_lector1.courses_attached += ['Python']
+cool_lector1.courses_attached += ['Java']
+cool_lector1.courses_attached += ['CSharp']
+cool_lector1.courses_attached += ['Pascal']
 
 
+best_student1.courses_in_progress += ['Python']
+best_student1.courses_in_progress += ['Java']
+best_student1.courses_in_progress += ['Pascal']
+best_student1.courses_in_progress += ['CSharp']
+best_student2.courses_in_progress += ['Python']
+best_student2.courses_in_progress += ['Pascal']
+best_student2.courses_in_progress += ['CSharp']
+best_student3.courses_in_progress += ['Python']
+best_student3.courses_in_progress += ['Pascal']
+best_student3.courses_in_progress += ['C++']
 
+best_student1.rate_lecturer(cool_lector, 'Python', 6)
+best_student2.rate_lecturer(cool_lector, 'Python', 10)
+best_student3.rate_lecturer(cool_lector, 'Python', 8)
 
-best_student1.courses_in_progress += ['Python'], ['Java'], ['Pascal'], ['CSharp']
-best_student2.courses_in_progress += ['Python'], ['Pascal'], ['CSharp']
-best_student3.courses_in_progress += ['Python'], ['Java'], ['C++']
-
-best_student1.rate_lecturer(cool_lector, ['Python'],10)
-best_student2.rate_lecturer(cool_lector, ['Python'],10)
-best_student3.rate_lecturer(cool_lector, ['Python'],10)
-
+best_student1.rate_lecturer(cool_lector1, 'Pascal', 7)
+best_student2.rate_lecturer(cool_lector1, 'Pascal', 6)
+best_student3.rate_lecturer(cool_lector1, 'Pascal', 8)
 
 
 cool_mentor.rate_hw(best_student1, 'Python', 8)
@@ -108,10 +141,18 @@ cool_mentor.rate_hw(best_student1, 'Java', 4)
 cool_mentor.rate_hw(best_student2, 'CSharp', 3)
 cool_mentor.rate_hw(best_student3, 'C++', 10)
 
-cool_mentor.rate_hw(best_student1, 'Java', 6)
+cool_mentor.rate_hw(best_student1, 'Pascal', 6)
 cool_mentor.rate_hw(best_student2, 'Pascal', 5)
-cool_mentor.rate_hw(best_student3, 'Java', 9)
+cool_mentor.rate_hw(best_student3, 'Pascal', 9)
 
 print(cool_mentor)
-
+print(best_student1)
+print(best_student2)
+print(best_student3)
+print(cool_lector)
+print(cool_lector1)
+print(best_student1 < best_student2)
+print(best_student1 < best_student3)
+print(cool_lector < cool_lector1)
+print(cool_lector1 < cool_lector)
 
